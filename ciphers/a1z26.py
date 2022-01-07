f3583bc9bd1cf8109cf4c671db75722b9ffd7af0
Sreenath Sreekrishna <94300241+SreenathSreekrishna@users.noreply.github.com>
2022-01-07 18:38:22 +0530

    Better explanation of code

--- ciphers/a1z26.py
+++ ciphers/a1z26.py
@@ -14,6 +14,10 @@ def encode(plain: str) -> list[int]:
     [13, 25, 14, 1, 13, 5]
     """
     return [ord(elem) - 96 for elem in plain]
+    """
+    ord(elem) - 96 ... -> gets ascii value of every character in plaintext and subtracting 96, therefore getting its place in the alpabet (eg: ord(m) -> 109, ord(m-96) -> 13)
+    [... for elem in plain] -> single line for loop iterating over the plaintext
+    """
 
 
 def decode(encoded: list[int]) -> str:
@@ -22,12 +26,20 @@ def decode(encoded: list[int]) -> str:
     'myname'
     """
     return "".join(chr(elem + 96) for elem in encoded)
+    """
+    "".join(...) -> converts list of substrings to single string (eg - ['m','y','n','a','m','e'] -> "myname")
+    chr(elem+96) ... -> adds 96 to ascii value of every element in encoded list and converts ascii to character (so 1 becomes 97, ascii value for 'a')
+    ... for elem in encoded -> single line for statement decoding every element "elem" in list of encoded characters "encoded"
+    """
 
 
 def main() -> None:
     encoded = encode(input("-> ").strip().lower())
+    #encoding input
     print("Encoded: ", encoded)
+    #printing encoded list
     print("Decoded:", decode(encoded))
+    #calling decode function on encoded list and printing the decoded list
 
 
 if __name__ == "__main__":
